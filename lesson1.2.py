760c69ade945e3cd7e3407cb2abc8924519876a0
PyPy-dot <nerosed131@gmail.com>
2024-10-01 22:10:02 +0500

    Add files via upload

---
+++ lesson1.2.py
@@ -0,0 +1,9 @@
+count_of_homeworks = 12
+
+hours = 1.5
+
+course_name = 'Python'
+
+one_lesson_time = hours / count_of_homeworks
+
+print(f'Курс: {course_name}, всего задач:{count_of_homeworks}, затрачено часов: {hours}, среднее время выполнения {one_lesson_time} часа.')
